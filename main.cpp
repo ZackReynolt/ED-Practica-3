c60fdefc57c94cd57d235361a5351316c430dec8
ZackReynolt <ZackReynolt@gmail.com>
2014-10-24 16:04:01 +0200

    First commit: Project creation.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: zackreynolt
+ *
+ * Created on 24 de octubre de 2014, 15:40
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
